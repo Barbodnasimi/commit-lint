13393367cc135d4165d8ab99f1f29f5604fe3f79
Barbodnasimi <barbod.nasimi.2006@gmail.com>
2024-03-02 04:34:25 -0800

    Feat-1: Add prompt to check a number
    Add prompt and function to check a number getting a number from the prompt and pass it to the function.
    
    Resolve: #1
    If applied, this commit will add a prompt to check a number

---
+++ script.js
@@ -0,0 +1,16 @@
+const input = prompt('enter a number')
+
+
+function checkNumber(num) {
+    if (isNaN(num)) {
+        alert('enter a number')
+    }else{
+        if (num > 50) {
+            alert('this number bigger than 50')
+        }else{
+            alert('this number smaller than 50')
+        }
+    }
+}
+
+checkNumber(input)
